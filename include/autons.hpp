5d2e7219ffa15368b21d4eb6ceae689987bbcc06
Acdhote <acdhote@gmail.com>
2025-08-13 20:28:48 -0400

    initial commit

---
+++ include/autons.hpp
@@ -0,0 +1,14 @@
+#pragma once
+
+void default_constants();
+
+void odom_test();
+void drive_example();
+void turn_example();
+void wait_until_change_speed();
+void swing_example();
+void motion_chaining();
+void combining_movements();
+void odom_drive_example();
+
+void measure_offsets();
