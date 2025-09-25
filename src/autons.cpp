ad483700c53a76bc79837f6705c31b2f5da69aa2
Kons <konstantinos32a@gmail.com>
2025-09-25 19:13:29 -0400

    commit

--- src/autons.cpp
+++ src/autons.cpp
@@ -19,7 +19,7 @@ void default_constants() {
   chassis.pid_heading_constants_set(11, 0.0, 32.0);        // Holds the robot straight while going forward without odom
   chassis.pid_turn_constants_set(3.75, 0.0, 30, 0);     // Turn in place constants
   chassis.pid_swing_constants_set(6.0, 0.0, 65.0);           // Swing constants
-  chassis.pid_odom_angular_constants_set(11.5, 1, 32, 10);    // Angular control for odom motions
+  chassis.pid_odom_angular_constants_set(11.5, 0.1, 32, 5);    // Angular control for odom motions
   chassis.pid_odom_boomerang_constants_set(5.2, 0.0, 40.5);  // Angular control for boomerang motions 
 
   // Exit conditions
